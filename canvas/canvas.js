0b386f9a2a1287761f619d12eab78e53037d3ad3
Iamaprogramador <104740094+Iamaprogramador@users.noreply.github.com>
2023-01-19 14:12:56 -0700

    Update canvas.js
    Make it so it actually does something

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,5 +1,11 @@
 function () {
-  const canvas = document.getElementById('gameCanvas')
-  const ctx = canvas.getContext('2d')
-
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  x=0;
+  y=0;
+  while () {
+    ctx.beginPath();
+    ctx.rect(x,y,1,1);
+    ctx.stroke();
 }
+    }
