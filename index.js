232c0eb846559eed1a7995f9fdf7e1770cba04a9
Iamaprogramador <104740094+Iamaprogramador@users.noreply.github.com>
2023-01-24 15:21:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 document.body.style.display = 'none';
 
-def promptPassword() {
-    var _password = prompt('Enter some text to display this page');
+function promptPassword() {
+    var _password = prompt('Enter some text to display this page. This do be a cookie.');
     //if (_password != '') promptPassword();
+    document.cookie = _password;
     document.body.style.display = 'default';
 }
+
+promptPassword();
+alert('Your text is now a cookie');
